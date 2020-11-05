ba70c0964cc1ed8d1309e83b1463e9bd13d034a3
abhilashajay-dev <55262545+abhilashajay-dev@users.noreply.github.com>
2020-11-05 13:59:18 +0530

    adde cart functionality

--- store/urls.py
+++ store/urls.py
@@ -3,6 +3,7 @@ from .views import (
 					Store,
 					Cart,
 					Checkout,
+					updateitem,
 
 )
 
@@ -10,5 +11,6 @@ urlpatterns = [
 path("", Store.as_view(), name='store'),
 path("cart/", Cart.as_view(), name='cart'),
 path("checkout/", Checkout.as_view(), name='checkout'),
+path("update_item/", updateitem, name='update_item'),
 
 ]
--- store/views.py
+++ store/views.py
@@ -1,22 +1,43 @@
 from django.shortcuts import render
 from django.views import generic
+from django.http import JsonResponse
 from .models import (
 						Product,
 						Order,
 						OrderItem,
 )
+import json
 # Create your views here.
 
 
 class Store(generic.ListView):
 	template_name = "store/store.html"
-	queryset = Product.objects.all()
+
 
 	def get_context_data(self, *args, **kwargs):
-		context = super().get_context_data(*args, **kwargs)
-		context["text"] = "this is the store"
-		context["products"] = context["object_list"] # parsing in built object_list to products for custom templating 
+		context = super(Store, self).get_context_data(*args, **kwargs)
+		context["products"] = self.object_list["products"]
+		context["items"] = self.object_list["items"] #--->Multiple context objects for rendering
+		context["order"] =self.object_list["order"]
+		context["cartItems"] = self.object_list["cartItems"]
+		# context["products"] = context["object_list"] # parsing in built object_list to products for custom templating 
 		return context
+
+	def get_queryset(self, *args, **kwargs):
+		request = self.request
+		if request.user.is_authenticated:
+			customer = request.user.customer #---> customer and user have OneToOne relationship
+			order, created = Order.objects.get_or_create(customer=customer, complete=False)
+			items = order.orderitem_set.all()
+			cartItems = order.get_cart_items
+		else:
+			items =[]
+			order= { "get_cart_total":0, "get_cart_items":0}
+			cartItems = order['get_cart_items']
+			
+		products = 	Product.objects.all()
+		return { "items": items, "order":order, "products":products, "cartItems":cartItems}
+
 
 
 
@@ -29,6 +50,7 @@ class Cart(generic.ListView):
 		context = super(Cart, self).get_context_data(*args, **kwargs)
 		context["items"] = self.object_list["items"] #--->Multiple context objects for rendering
 		context["order"] =self.object_list["order"]
+		context["cartItems"] = self.object_list["cartItems"]
 		return context
 
 	def get_queryset(self, *args, **kwargs):
@@ -37,10 +59,12 @@ class Cart(generic.ListView):
 			customer = request.user.customer #---> customer and user have OneToOne relationship
 			order, created = Order.objects.get_or_create(customer=customer, complete=False)
 			items = order.orderitem_set.all()
+			cartItems = order.get_cart_items
 		else:
 			items =[]
-			order= { "get_cart_total":0, "get_cart_items":0 }	
-		return { "items": items, "order":order}
+			order= { "get_cart_total":0, "get_cart_items":0 }
+			cartItems = order['get_cart_items']	
+		return { "items": items, "order":order, "cartItems":cartItems}
 
 
 
@@ -52,4 +76,31 @@ class Checkout(Cart):
 		context = super(Checkout, self).get_context_data(*args, **kwargs)
 		context["text"] = "this is the checkout"
 		return context
+
+# update item view
 	
+def updateitem(request):
+	data = json.loads(request.body)
+	productId = data['productId']
+	action = data['action']
+	
+	print('Action:', action)
+	print('Product:', productId)
+
+	customer = request.user.customer
+	product = Product.objects.get(id=productId)
+	order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+	orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+	if action == 'add':
+		orderItem.quantity = (orderItem.quantity + 1)
+	elif action == 'remove':
+		orderItem.quantity = (orderItem.quantity - 1)
+
+	orderItem.save()
+
+	if orderItem.quantity <= 0:
+		orderItem.delete()
+
+	return JsonResponse('Item was added', safe=False)
